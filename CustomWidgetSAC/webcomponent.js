2c9811133225498fe98eeffed6fd10c1b586d9a6
Gentlemanlike82 <MarcSchweizer@gmx.de>
2020-05-20 15:40:33 +0200

    change in Webcomponent
    Change-Id: I3aed9ffda942d134584b5abbc493fc5631bd2917

--- CustomWidgetSAC/webcomponent.js
+++ CustomWidgetSAC/webcomponent.js
@@ -1,8 +1,9 @@
 (function()  {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
+          <h1>Hello World</h1>
       <style>
-      <h1>Hello World</h1>
+
       </style>
     `;
 
@@ -11,8 +12,9 @@
 
 		constructor() {
 			super(); 
-			let shadowRoot = this.attachShadow({mode: "open"});
-			shadowRoot.appendChild(tmpl.content.cloneNode(true));
+			this._shadowRoot = this.attachShadow({mode: "open"});
+            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this._firstConnection = false;
 		}
 
 
